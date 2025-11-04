71348f6515465bc51493128e425bd48625dedc83
Jitkal Mitrakul <66130500113@ad.sit.kmutt.ac.th>
2025-11-04 21:41:56 +0700

    set up router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import './App.css'
 import Navbar from "./components/Navbar.jsx";
+import Home from "./pages/Home.jsx";
 import ToDoLists from "./pages/ToDoLists.jsx";
 
 function App() {
@@ -7,8 +9,13 @@ function App() {
   return (
     <div className="w-full h-full flex flex-col mx-auto">
         <div className='px-32'>
-            <Navbar/>
-            <ToDoLists/>
+            <BrowserRouter>
+                <Navbar/>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/todo" element={<ToDoLists />} />
+                </Routes>
+            </BrowserRouter>
         </div>
     </div>
   )
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 
 const Navbar = () => {
   return (
@@ -13,12 +15,14 @@ const Navbar = () => {
                   <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                       <li>
                           <div className="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">
-                              Home
+
+                              <Link to="/">Home</Link>
                           </div>
                       </li>
                       <li>
                       <div className="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">
-                          To Do List
+                          <Link to="/todo">To Do List</Link>
+
                       </div>
                   </li>
                   </ul>
